bd4ce5b83bcdd8b057107f52d510523b9f975e60
Sebastien Klay <sebklay@hotmail.com>
2026-03-16 12:56:04 +0100

    updated list models

--- app/utils/listModels.ts
+++ app/utils/listModels.ts
@@ -6,22 +6,14 @@ export interface IModelList {
 export const MODELS: Record<number, IModelList> = {
   1: {
     id: 1,
-    model_name: "llama3.2:3b",
+    model_name: "llama3.1:8b",
   },
   2: {
     id: 2,
-    model_name: "tinyllama",
+    model_name: "mistral-small:24b",
   },
   3: {
     id: 3,
-    model_name: "gemma3:4b",
-  },
-  4: {
-    id: 4,
-    model_name: "qwen3-vl:2b",
-  },
-  5: {
-    id: 5,
-    model_name: "moondream:1.8b",
-  },
+    model_name: "qwen2.5:32b",
+  }
 };
